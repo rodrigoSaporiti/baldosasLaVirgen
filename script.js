df5d91628f96f1fef7ad95763ba701e676958dde
RodrigoSaporiti <rodrigosaporiti6949@gmail.com>
2023-10-15 02:33:58 -0300

    Catalogo listo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-
+/*
  document.addEventListener("DOMContentLoaded", function() {
         let miModal = new bootstrap.Modal(document.getElementById('miModal'));
         miModal.show();
     });
 
+*/
