ae1defde64a1c1d9ecd88bec1978ccd02aa20f7d
RodrigoSaporiti <rodrigosaporiti6949@gmail.com>
2023-11-30 22:31:08 -0300

    algunos cambios

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-/*
+
  document.addEventListener("DOMContentLoaded", function() {
         let miModal = new bootstrap.Modal(document.getElementById('miModal'));
         miModal.show();
     });
-
-*/
