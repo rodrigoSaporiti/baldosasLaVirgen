793b403dbb8a3423d73460e88d52836f32a4e63e
Infra-Anima <81244183+PerseoES@users.noreply.github.com>
2023-12-06 18:19:43 -0300

    Gallery 2.0
    Casi terminada, faltan botones para transicionar de imágenes y el responsive para mobile.

--- frontend/galeria.js
+++ frontend/galeria.js
@@ -22,3 +22,31 @@ function redirigirABanos() {
     console.log("Redirigiendo a 'interiores.html'");
     window.location.href = 'interiores.html';
   }
+
+// Inicio de interacción para las fotos en la galería, se agrega transición cada vez que se clickea la imágen, se envían los atributos de source (ruta de la imágen) y alt que es el texto alternativo por si no se visualiza la foto.
+  const imagenes = document.querySelectorAll(".img");
+
+  const containerImage = document.querySelector(".containerImg");
+
+  const imgClickeada = document.querySelectorAll('.imgShow');
+
+  const copy = document.querySelector('.copy');
+
+  imagenes.forEach(imagen => {
+    imagen.addEventListener("click", ()=>{
+      agregarImagen(imagen.getAttribute('src'), imagen.getAttribute('alt'));
+    })
+
+  })
+
+  const agregarImagen = (srcImagen, altImagen)=>{
+    containerImage.classList.toggle('moveContainer');
+    containerImage.src = srcImagen;
+    copy.innerHTML = altImagen;
+  };
+
+  containerImage.addEventListener('click', ()=>{
+    containerImage.classList.toggle('moveContainer');
+  })
+
+  // Fin interacción de galería.
