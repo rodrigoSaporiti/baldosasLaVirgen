c4ff2a7de5ad5cd21b5df4aded8991c1f18c9a46
RodrigoSaporiti <rodrigosaporiti6949@gmail.com>
2023-12-05 21:05:58 -0300

    cambios

---
+++ frontend/galeria.js
@@ -0,0 +1,24 @@
+function redirigirABanos() {
+    console.log("Redirigiendo a 'baños.html'");
+    window.location.href = 'baños.html';
+  }
+
+  function redirigirACocinas() {
+    console.log("Redirigiendo a 'Cocinas.html'");
+    window.location.href = 'cocinas.html';
+  }
+
+  function redirigirALocalesComerciales() {
+    console.log("Redirigiendo a 'localesComerciales.html'");
+    window.location.href = 'localesComerciales.html';
+  }
+
+  function redirigirAExteriores() {
+    console.log("Redirigiendo a 'exteriores.html'");
+    window.location.href = 'exteriores.html';
+  }
+  
+  function redirigirAInteriores() {
+    console.log("Redirigiendo a 'interiores.html'");
+    window.location.href = 'interiores.html';
+  }
