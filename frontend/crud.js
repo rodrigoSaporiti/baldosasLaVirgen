1fa78289e1cb748714fd0ad74ef81bf7a672d31e
rodrigoSaporiti <rodrigosaporiti6949@gmail.com>
2024-01-04 18:05:00 -0300

    crud falta mosaico y responsive

---
+++ frontend/crud.js
@@ -0,0 +1,24 @@
+function redirigirABanos() {
+    console.log("Redirigiendo a 'baños.html'");
+    window.location.href = 'bañosCrud.html';
+  }
+
+  function redirigirACocinas() {
+    console.log("Redirigiendo a 'Cocinas.html'");
+    window.location.href = 'cocinasCrud.html';
+  }
+
+  function redirigirALocalesComerciales() {
+    console.log("Redirigiendo a 'localesComerciales.html'");
+    window.location.href = 'localesComercialesCrud.html';
+  }
+
+  function redirigirAExteriores() {
+    console.log("Redirigiendo a 'exteriores.html'");
+    window.location.href = 'exterioresCrud.html';
+  }
+  
+  function redirigirAInteriores() {
+    console.log("Redirigiendo a 'interioresCrud.html'");
+    window.location.href = 'interioresCrud.html';
+  }
