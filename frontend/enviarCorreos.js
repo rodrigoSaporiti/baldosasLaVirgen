346393fad756ea1f420ac7afc84db221dab00fea
RodrigoSaporiti <rodrigosaporiti6949@gmail.com>
2024-01-25 20:26:36 -0300

    falta, contacto correo enviar, y todo lo del crud, con session y token

--- frontend/enviarCorreos.js
+++ frontend/enviarCorreos.js
@@ -13,57 +13,60 @@ function validarEmail(email) {
 const enviarCorreo = document.getElementById("enviarCorreo");
 
 
+// Modal de agradecimiento
+
+function mostrarModal() {
+  let modal = document.querySelector(".contenidoModal");
+
+  
+  modal.style.display = "flex";
+
+  
+  setTimeout(() => {
+    modal.style.display = "none";
+    location.reload()
+  }, 1500); 
 
 
-
-function crudCorreo(){
-
-enviarCorreo.addEventListener("click", ()=>{
-
-    alert("Gracias por Suscribirte")
-
-
- location.reload();
-
- const correoNuevo = document.getElementById("correo").value;
-
-
-
-if(validarEmail(correoNuevo)){
-
- console.log(correoNuevo);
-
-  return fetch('http://localhost:3000/enviarCorreo', {
-   method: 'POST',
-   headers: {
-      'Content-Type': 'application/json',
-       },
-    body: JSON.stringify({correoNuevo}),
-})
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => {
-     console.error('Error en la solicitud:', error);
-     return { error: true };
-     
-
-     
-
-})
-
-}else{
-
-alert("Ingrese un correo valido")
-
+ 
 }
 
 
 
 
-})
+function crudCorreo() {
 
+  enviarCorreo.addEventListener("click", () => {
+    
 
+    const correoNuevo = document.getElementById("correo").value;
 
+    if (validarEmail(correoNuevo)) {
+
+      mostrarModal();
+      
+      fetch('http://localhost:3000/enviarCorreo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ correoNuevo }),
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // Si deseas recargar después de completar la operación asíncrona, pon la recarga aquí.
+        
+       
+      })
+      .catch(error => {
+        console.error('Error en la solicitud:', error);
+        return { error: true };
+      });
+    } else {
+      alert("Ingrese un correo válido");
+    }
+  });
 }
 
 crudCorreo();
